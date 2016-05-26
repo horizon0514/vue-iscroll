46d1a5dc8c614d87eef2d49c5d109375a03c5687
horizon0514 <horizon0514@gmail.com>
2016-05-26 18:00:57 +0800

    init

---
+++ src/scroll.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="wrapper">
+		<div class="scroller">
+			<slot></slot>
+			<slot name="pulldown"></slot>
+      <slot name="pullup"></slot>
+		</div>
+	</div>
+</template>
+<script>
+	var iScroll = require('iscroll/build/iscroll-lite')
+
+	export default{
+		name: "vue-iscroller",
+		props:{
+
+		},
+
+		
+	}
+</script>
+<style scoped>
+	.wrapper{
+		position: absolute;
+    z-index: 1;
+    top: 0;
+    bottom: 48px;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+	}
+</style>
